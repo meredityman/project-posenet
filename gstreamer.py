93d3466cc38ab1a7a8825bbc7ea02265836bea0a
Meredith Thomas <meredityman@gmail.com>
2020-07-02 19:07:08 +0200

    wc

--- gstreamer.py
+++ gstreamer.py
@@ -350,7 +350,7 @@ def run_pipeline(inf_callback, render_callback, src_size,
         scale_caps = 'video/x-raw,width={width},height={height}'.format(width=scale[0], height=scale[1])
         PIPELINE += """ ! decodebin ! videoflip video-direction={direction} ! tee name=t
                t. ! {leaky_q} ! videoconvert ! freezer name=freezer ! rsvgoverlay name=overlay
-                  ! videoconvert ! glvideosink
+                  ! videoconvert ! glimagessink
                t. ! {leaky_q} ! videoconvert ! videoscale ! {scale_caps} ! videobox name=box autocrop=true
                   ! {sink_caps} ! {sink_element}
             """
